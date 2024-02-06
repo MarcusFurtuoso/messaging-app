da8d20409d68d71e15f49b096049b51fc9d306cb
MarcusFurtuoso <marcusvini678@gmail.com>
2024-02-05 22:34:01 -0300

    implemented register form

--- src/app/register-contact/register-contact.component.ts
+++ src/app/register-contact/register-contact.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-contact',
   templateUrl: './register-contact.component.html',
-  styleUrls: ['./register-contact.component.css']
+  styleUrls: ['./register-contact.component.css'],
 })
-export class RegisterContactComponent {
+export class RegisterContactComponent implements OnInit {
+  registerForm!: FormGroup;
 
-  constructor(private router: Router) { }
+  errorMessages = {
+    firstName: {
+      required: 'First name is required',
+      minlength: 'First name should be min 2 chars long',
+    },
+    lastName: {
+      required: 'Last name is required',
+      minlength: 'Last name should be min 2 chars long',
+    },
+    company: {
+      required: 'Company is required',
+    },
+    mobile: {
+      required: 'Mobile is required',
+    },
+    email: {
+      required: 'Email is required',
+      email: 'Email is invalid',
+    },
+  };
+
+  constructor(private router: Router, public formBuilder: FormBuilder) {}
 
   openPage(page: string) {
     switch (page) {
@@ -18,4 +41,37 @@ export class RegisterContactComponent {
       default:
     }
   }
+
+  ngOnInit() {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      company: ['', [Validators.required, Validators.minLength(2)]],
+      mobile: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+        ],
+      ],
+    });
+  }
+
+  get errorControl() {
+    return this.registerForm.controls;
+  }
+
+  submitForm = () => {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      return false;
+    } else {
+      return console.log('Please provide all the required values!');
+    }
+  };
 }
--- src/app/register-contact/register-contact.module.ts
+++ src/app/register-contact/register-contact.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterContactComponent } from './register-contact.component';
 import { RegisterContactRoutingModule } from './register-contact-routing.module';
 
@@ -11,6 +11,7 @@ import { RegisterContactRoutingModule } from './register-contact-routing.module'
     IonicModule,
     FormsModule,
     RegisterContactRoutingModule,
+    ReactiveFormsModule,
   ],
   declarations: [RegisterContactComponent]
 })
